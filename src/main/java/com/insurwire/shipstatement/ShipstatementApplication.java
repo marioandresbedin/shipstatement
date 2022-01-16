9e2d10cfcf78cd1d6b8c2e1eab88b8850989e8e1
Mario Bedin <mario.andres.bedin@gmail.com>
2022-01-16 09:03:11 -0300

    commit base application

---
+++ src/main/java/com/insurwire/shipstatement/ShipstatementApplication.java
@@ -0,0 +1,13 @@
+package com.insurwire.shipstatement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipstatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipstatementApplication.class, args);
+	}
+
+}
